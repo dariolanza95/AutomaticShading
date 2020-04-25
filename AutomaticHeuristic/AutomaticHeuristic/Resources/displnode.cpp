ca371cf45d48e5923a8c8abc6f9658965a31c53e
Dar <dario.lanza95@gmail.com>
2020-04-25 15:43:42 +0200

    RIB file creation implemented

---
+++ AutomaticHeuristic/AutomaticHeuristic/Resources/displnode.cpp
@@ -0,0 +1,6 @@
+#include "displnode.h"
+
+DisplNode::DisplNode()
+{
+
+}
---
+++ AutomaticHeuristic/AutomaticHeuristic/Resources/displnode.h
@@ -0,0 +1,11 @@
+#ifndef DISPLNODE_H
+#define DISPLNODE_H
+
+
+class DisplNode
+{
+public:
+    DisplNode();
+};
+
+#endif // DISPLNODE_H
